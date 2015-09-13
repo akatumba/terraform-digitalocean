7bebffc76e2b24919a7b12e482ecd148c1db6d8e
Thomas <thomas.lierfeld@gmail.com>
2015-09-13 19:27:57 +0200

    added variables.tf for variables. moved variables from provider.tf to variables.tf and added descriptions

--- domain.tf
+++ domain.tf
@@ -1,6 +1,6 @@
 # Create a new domain record
 resource "digitalocean_domain" "default-domain" {
-   name = "example.com"
+   name = "www.example.com"
    ip_address = "${digitalocean_droplet.ubuntu-www.ipv4_address}"
 }
 
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "do_token" {
+	description = "Digital Ocean Access Key"
+}
+
+variable "pub_key" {
+	description = "Path to your public key. e.g. ~/.ssh/digitalocean_rsa.pub"
+}
+
+variable "pvt_key" {
+	description = "Path to your public key. e.g. ~/.ssh/digitalocean_rsa"
+}
+
+variable "ssh_fingerprint" {
+	description = "SSH fingerprint. "
+}
